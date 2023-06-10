b8f255596a10aca99db029e9b57a2b7640adc365
edealfonso <elsa.de.alfonso@gmail.com>
2023-06-10 13:49:18 +0200

    env api

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
     sassOptions: {
         prependData: `@import "./styles/settings/__settings.scss";`
     }
+    // env: {
+    //     BACKEND_SERVER_URL: process.env.BACKEND_SERVER_URL,
+    // },
 };
 
 module.exports = nextConfig;
