7ba0f7215c69c1486b8f8475264e283d1f8990d7
edealfonso <elsa.de.alfonso@gmail.com>
2023-06-28 23:48:35 +0200

    erase debug tests

--- components/record/AudioRecorder.js
+++ components/record/AudioRecorder.js
@@ -5,10 +5,10 @@ import { preparePlayer, stopAudio } from '@/lib/audio';
 import { useWindowDimensions } from '@/lib/hooks/useWindowDimensions';
 
 import dynamic from 'next/dynamic';
-// const AudioAnalyser = dynamic(import('react-audio-analyser'), { ssr: false }); // Async API cannot be server-side rendered
-const AudioAnalyser = dynamic(import('@/lib/react-audio-analyser'), {
-    ssr: false
-}); // Async API cannot be server-side rendered
+const AudioAnalyser = dynamic(import('react-audio-analyser'), { ssr: false }); // Async API cannot be server-side rendered
+// const AudioAnalyser = dynamic(import('@/lib/react-audio-analyser'), {
+//     ssr: false
+// }); // Async API cannot be server-side rendered
 
 import styles from './AudioRecorder.module.scss';
 
